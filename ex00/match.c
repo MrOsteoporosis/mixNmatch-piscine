ba226b546ed992e128094dcf7f1f5057b493853a
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-22 10:52:02 +0200

    ex00 rewritten into two functions

--- ex00/match.c
+++ ex00/match.c
@@ -6,9 +6,28 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/22 10:18:08 by averheij       #+#    #+#                */
-/*   Updated: 2019/09/22 10:36:47 by averheij      ########   odam.nl         */
+/*   Updated: 2019/09/22 10:50:48 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		ft_dealwiththisbullshit(char *s1, char *s2, int pos1, int pos2)
+{
+	while (s2[pos2] == '*' && s1[pos1])
+	{
+		if (!s2[pos2 + 1])
+		{
+			while (s1[pos1])
+				pos1++;
+			pos2++;
+			break ;
+		}
+		if (s1[pos1] == s2[pos2 + 1])
+			pos2++;
+		else
+			pos1++;
+	}
+	return (pos1);
+}
 
 int		match(char *s1, char *s2)
 {
@@ -19,21 +38,12 @@ int		match(char *s1, char *s2)
 	pos2 = 0;
 	while (s1[pos1] && s2[pos2])
 	{
-		while (s2[pos2] == '*' && s1[pos1])
+		if (s2[pos2] == '*')
 		{
 			while (s2[pos2 + 1] == '*')
 				pos2++;
-			if (!s2[pos2 + 1])
-			{
-				while (s1[pos1])
-					pos1++;
-				pos2++;
-				break ;
-			}
-			if (s1[pos1] == s2[pos2 + 1])
-				pos2++;
-			else
-				pos1++;
+			pos1 = ft_dealwiththisbullshit(s1, s2, pos1, pos2);
+			pos2++;
 		}
 		if (s1[pos1] != s2[pos2])
 			return (0);
