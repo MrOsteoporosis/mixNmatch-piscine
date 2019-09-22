19968961d60e8bcb7a98516d4a4b4713297f62fd
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-22 10:39:43 +0200

    kms

--- ex00/main.c
+++ ex00/main.c
@@ -1,9 +1,21 @@
-int     match(char *s1, char *s2);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/22 10:17:58 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/22 10:19:28 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-int     main(int argc, char **argv)
+int		match(char *s1, char *s2);
+
+int		main(int argc, char **argv)
 {
-    if (argc == 3)
-        return(match(argv[1], argv[2]));
-    else
-        return (-1);    
+	if (argc == 3)
+		return (match(argv[1], argv[2]));
+	else
+		return (-1);
 }
--- ex00/match.c
+++ ex00/match.c
@@ -1,25 +1,44 @@
-int     match(char *s1, char *s2)
-{
-    int     pos1;
-    int     pos2;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   match.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/22 10:18:08 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/22 10:36:47 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-    pos1 = 0;
-    pos2 = 0;
-    while (s1[pos1] || s2[pos2])
-    {
-        while (s2[pos2] == '*')
-        {
-            while(s2[pos2 + 1] == '*')
-                pos2++;
-            if(s1[pos1] == s2[pos2 + 1])
-                pos2++;
-            else
-                pos1++;
-        }
-        if (s1[pos1] != s2[pos2] || !s1[pos1] || !s2[pos2])
-            return(0);
-        pos1++;
-        pos2++;
-    }
-    return (1);
+int		match(char *s1, char *s2)
+{
+	int		pos1;
+	int		pos2;
+
+	pos1 = 0;
+	pos2 = 0;
+	while (s1[pos1] && s2[pos2])
+	{
+		while (s2[pos2] == '*' && s1[pos1])
+		{
+			while (s2[pos2 + 1] == '*')
+				pos2++;
+			if (!s2[pos2 + 1])
+			{
+				while (s1[pos1])
+					pos1++;
+				pos2++;
+				break ;
+			}
+			if (s1[pos1] == s2[pos2 + 1])
+				pos2++;
+			else
+				pos1++;
+		}
+		if (s1[pos1] != s2[pos2])
+			return (0);
+		pos1++;
+		pos2++;
+	}
+	return (1);
 }
